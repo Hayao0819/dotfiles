57f04785e9908a42a45a19001c29cdfd999547e2
hayao <shun819.mail@gmail.com>
2024-10-03 17:29:06 +0900

    Add: Nix config

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS + standalone home-manager config flakes to get you started!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+  in {
+    templates = {
+      standard = {
+        description = ''
+          Standard flake - augmented with boilerplate for custom packages, overlays, and reusable modules.
+          Perfect migration path for when you want to dive a little deeper.
+        '';
+        path = ./standard;
+      };
+    };
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
+}
--- shell/bash_profile
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# ~/.bash_profile
-#
-
-# shellcheck disable=SC1090
-[[ -f ~/.bashrc ]] && . ~/.bashrc
