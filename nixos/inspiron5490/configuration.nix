d2c4f8b7d42c20d835ea08daa69dca17cba3d6b8
hayao <hayao@fascode.net>
2024-10-04 12:45:21 +0900

    Add: Nix modules

--- nixos/inspiron5490/configuration.nix
+++ nixos/inspiron5490/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "Inspiron5450"; # Define your hostname.
+  networking.hostName = "Inspiron5490"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
