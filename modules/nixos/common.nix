9441204419c5bea49b920e94748890084b687132
hayao <hayao@fascode.net>
2024-12-07 17:22:30 +0900

    Add: Split common files

---
+++ modules/nixos/common.nix
@@ -0,0 +1,23 @@
+{ ...
+}: {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+
+}
--- nixos/inspiron5490/configuration.nix
+++ nixos/inspiron5490/configuration.nix
@@ -2,11 +2,6 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-
-# TODO:
-# - Import network module
-# - Import locale module
-
 { config, pkgs, outputs, inputs, ... }:
 
 {
@@ -17,6 +12,7 @@
       outputs.nixosModules.network
       outputs.nixosModules.locale
       outputs.nixosModules.gnome
+      outputs.nixosModules.common
 
       # Or modules from other flakes (such as nixos-hardware):
       # inputs.hardware.nixosModules.common-cpu-amd
@@ -35,23 +31,6 @@
 
   networking.hostName = "Inspiron5490"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -77,9 +56,6 @@
     };
   };
 
-  # Don't ask for password
-  security.sudo.wheelNeedsPassword = false;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -92,7 +68,6 @@
     #  wget
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
