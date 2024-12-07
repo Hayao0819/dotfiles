ef4ffe5484ecceafdeafec42e7e011fb4e60e472
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-12-07 21:52:20 +0500

    chore: follow nix norms of aliases

--- home/darwin.nix
+++ home/darwin.nix
@@ -11,9 +11,9 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
-    outputs.homeManagerModules.git
-    outputs.homeManagerModules.zsh
-    outputs.homeManagerModules.pkgs
+    outputs.homeModules.git
+    outputs.homeModules.zsh
+    outputs.homeModules.pkgs
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
