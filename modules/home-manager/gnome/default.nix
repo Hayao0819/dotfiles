347e22ddf66c32a1be96a20b936318e8d44064c0
hayao <hayao@fascode.net>
2024-12-08 18:16:03 +0900

    update: Add dconf

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -3,10 +3,17 @@
   dconf = {
     enable = true;
     settings."org/gnome/shell" = {
+      # Enable dark mode
       disable-user-extensions = false;
+
+      # Enable dark mode
+      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+      # Enable extensions
       enabled-extensions = with pkgs.gnomeExtensions; [
-        blur-my-shell.extensionUuid
         gsconnect.extensionUuid
+        arcmenu.extensionUuid
+        dash-to-panel.extensionUuid
       ];
     };
   };
