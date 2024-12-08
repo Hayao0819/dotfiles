be84022ef50ec5171b43e23b952e8e2b04177a7d
hayao <hayao@fascode.net>
2024-12-08 18:38:28 +0900

    fix: dark theme

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -6,15 +6,17 @@
       # Enable dark mode
       disable-user-extensions = false;
 
-      # Enable dark mode
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-
       # Enable extensions
       enabled-extensions = with pkgs.gnomeExtensions; [
         gsconnect.extensionUuid
         arcmenu.extensionUuid
         dash-to-panel.extensionUuid
       ];
+    };
+
+    # Enable dark mode
+    settings."org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
     };
   };
 }
