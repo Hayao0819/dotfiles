97cf9ee4ddabe2cb2d806ecb18ebdcb1cb97852a
hayao <hayao@fascode.net>
2024-12-08 23:11:41 +0900

    add: darwin

---
+++ darwin/default.nix
@@ -0,0 +1,13 @@
+{ nix-darwin, inputs, outputs, ... }: {
+
+  MacBook = nix-darwin.lib.darwinSystem {
+    system = "aarch64-darwin";
+    modules = [
+      ./macbook/configuration.nix
+    ];
+    specialArgs = {
+      inherit inputs outputs;
+    };
+  };
+
+}
--- flake.nix
+++ flake.nix
@@ -1,7 +1,25 @@
 {
   description = "Your new nix config";
 
-  inputs = import ./upstream.nix;
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    # Nix-darwin for macOS systems management
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
     { self
@@ -60,20 +78,7 @@
       # Darwin configuration entrypoint
       # Available through 'darwin-rebuild build --flake .#your-hostname'
       # Stored at/as root/darwin/<alias name for machine>/*.nix
-      darwinConfigurations = self.lib.config.attrSystem {
-        inherit inputs outputs;
-        type = "darwin";
-        list = [
-          {
-            name = "Sokhibjons-MacBook-Pro";
-            alias = "macbook-pro";
-          }
-          {
-            name = "Sokhibjons-Mac-Studio";
-            alias = "mac-studio";
-          }
-        ];
-      };
+      darwinConfigurations = import ./darwin { inherit inputs outputs; };
 
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
